518fa12aa89227c72ba2e0b25017bbabd08ec121
Jefferson <90939375+JeffersonAlves7@users.noreply.github.com>
2025-04-09 23:30:34 -0300

    feat: authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
-import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, AuthModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
